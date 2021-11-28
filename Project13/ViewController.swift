4c6f41bbf8861c695635cb9abc7f12e4267554d0
DmitriiMM <martinovit72@gmail.com>
2021-11-28 12:50:09 +0500

    Initial Commit

---
+++ Project13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project13
+//
+//  Created by Дмитрий Мартынов on 28.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
